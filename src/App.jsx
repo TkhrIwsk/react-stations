0b3e5ce5ed69c8b70c90ded3237f22d314a6ec7f
TkhrIwsk <taka_0119volleyball@yahoo.co.jp>
2025-08-23 22:01:36 +0900

    useStateの設定

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,16 @@ import './App.css'
  * @type {() => JSX.Element}
  */
 export const App = () => {
+const[dogUrl, setDogUrl] = useState(
+    "https://images.dog.ceo/breeds/terrier-russell/jack-koda-1.jpg"
+  );
+
   return (
     <div>
       <header>アプリ</header>
       <main>
         <img 
-          src="https://images.dog.ceo/breeds/terrier-russell/jack-koda-1.jpg"
+          src={dogUrl}   // state を利用
           width="400"
           alt="犬の画像"
         />
